621a807bdc8666695dd01973602a6f13c66d8917
GabrielReynaldo <gabrielreynaldo123@gmai.com>
2022-05-01 20:31:55 -0300

    projeto iniciado

---
+++ src/main/java/com/projeto/MotoLivre/MotoLivreApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.MotoLivre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotoLivreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MotoLivreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projeto/MotoLivre/MotoLivreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.MotoLivre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotoLivreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
