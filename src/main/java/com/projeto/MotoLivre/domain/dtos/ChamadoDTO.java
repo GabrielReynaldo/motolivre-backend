6e2fec929afaba74e787fb0c9f84922fda8f5427
GabrielReynaldo <gabrielreynaldo123@gmai.com>
2022-05-20 20:52:19 -0300

    findAll para chamado

--- src/main/java/com/projeto/MotoLivre/domain/dtos/ChamadoDTO.java
+++ src/main/java/com/projeto/MotoLivre/domain/dtos/ChamadoDTO.java
@@ -1,5 +1,133 @@
 package com.projeto.MotoLivre.domain.dtos;
 
-public class ChamadoDTO {
+import java.io.Serializable;
+import java.time.LocalDate;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.projeto.MotoLivre.domain.Chamado;
+
+public class ChamadoDTO  implements Serializable{
+	private static final long serialVersionUID = 1L;
+
+private Integer id;
+	
+	@JsonFormat(pattern = "dd/MM/yyyy")
+	private LocalDate dataAbertuda = LocalDate.now();
+	@JsonFormat(pattern = "dd/MM/yyyy")
+	private LocalDate dataFechamento;
+	private Integer prioridade;
+	private Integer status;
+	private String titulo;
+	private String observacoes;
+	private Integer motoboy;
+	private Integer estabelecimento;
+	private String nomeEstabelecimento;
+	private String nomeMotoboy;
+	public ChamadoDTO() {
+		super();
+	}
+	
+	public ChamadoDTO( Chamado obj) {
+		this.id = obj.getId();
+		this.dataAbertuda = obj.getDataAbertuda();
+		this.dataFechamento = obj.getDataFechamento();
+		this.prioridade = obj.getPrioridade().getCodigo();
+		this.status = obj.getStatus().getCodigo();
+		this.titulo = obj.getTitulo();
+		this.observacoes = obj.getObservacoes();
+		this.motoboy = obj.getMotoboy().getId();
+		this.estabelecimento = obj.getEstabelecimento().getId();
+		this.nomeEstabelecimento = obj.getEstabelecimento().getNome();
+		this.nomeMotoboy = obj.getMotoboy().getNome();
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public LocalDate getDataAbertuda() {
+		return dataAbertuda;
+	}
+
+	public void setDataAbertuda(LocalDate dataAbertuda) {
+		this.dataAbertuda = dataAbertuda;
+	}
+
+	public LocalDate getDataFechamento() {
+		return dataFechamento;
+	}
+
+	public void setDataFechamento(LocalDate dataFechamento) {
+		this.dataFechamento = dataFechamento;
+	}
+
+	public Integer getPrioridade() {
+		return prioridade;
+	}
+
+	public void setPrioridade(Integer prioridade) {
+		this.prioridade = prioridade;
+	}
+
+	public Integer getStatus() {
+		return status;
+	}
+
+	public void setStatus(Integer status) {
+		this.status = status;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getObservacoes() {
+		return observacoes;
+	}
+
+	public void setObservacoes(String observacoes) {
+		this.observacoes = observacoes;
+	}
+
+	public Integer getMotoboy() {
+		return motoboy;
+	}
+
+	public void setMotoboy(Integer motoboy) {
+		this.motoboy = motoboy;
+	}
+
+	public Integer getEstabelecimento() {
+		return estabelecimento;
+	}
+
+	public void setEstabelecimento(Integer estabelecimento) {
+		this.estabelecimento = estabelecimento;
+	}
+
+	public String getNomeEstabelecimento() {
+		return nomeEstabelecimento;
+	}
+
+	public void setNomeEstabelecimento(String nomeEstabelecimento) {
+		this.nomeEstabelecimento = nomeEstabelecimento;
+	}
+
+	public String getNomeMotoboy() {
+		return nomeMotoboy;
+	}
+
+	public void setNomeMotoboy(String nomeMotoboy) {
+		this.nomeMotoboy = nomeMotoboy;
+	}
+	
+	
 }
--- src/main/java/com/projeto/MotoLivre/services/ChamadoService.java
+++ src/main/java/com/projeto/MotoLivre/services/ChamadoService.java
@@ -1,5 +1,27 @@
 package com.projeto.MotoLivre.services;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.projeto.MotoLivre.domain.Chamado;
+import com.projeto.MotoLivre.repositories.ChamadoRepository;
+import com.projeto.MotoLivre.services.exceptions.ObjectnotFoundException;
+
+@Service
 public class ChamadoService {
 
+	@Autowired
+	private ChamadoRepository repository;
+	
+	public Chamado findById(Integer id) {
+		Optional<Chamado> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectnotFoundException("Objeto não encontrado ID" + id));
+	}
+
+	public List<Chamado> findAll() {
+		return repository.findAll();
+	}
 }
